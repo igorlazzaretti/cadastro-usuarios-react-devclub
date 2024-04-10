e93e9fe1298c5f2aa9b9332589788541e13f628a
Igor Dossin Lazzaretti <idlazzaretti@gmail.com>
2024-04-10 16:02:46 -0300

    commit

---
+++ src/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div `
+
+background-color: red;
+
+
+`
+export const H1 = styled.h1`
+
+color: blue;
+font-size: 020px;
+`
